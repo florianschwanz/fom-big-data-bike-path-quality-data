4b5a01c38062f1fe978a7f68931a37c301828969
Florian Schwanz <florian.schwanz@interoberlin.de>
2021-06-18 17:11:54 +0200

    feat: add main script to Github action #1

---
+++ main.py
@@ -0,0 +1,26 @@
+import os
+import sys
+
+# Make library available in path
+library_paths = [
+    os.path.join(os.getcwd(), 'lib')
+]
+
+for p in library_paths:
+    if not (p in sys.path):
+        sys.path.insert(0, p)
+
+# Import library classes
+from data_downloader_firebase_storage import FirebaseStorageDownloader
+
+# Configuration
+RELOAD_DATA = True
+
+# Set script path
+file_path = os.path.realpath(__file__)
+script_path = os.path.dirname(file_path)
+
+# Download data from Firebase Storage
+FirebaseStorageDownloader().run(
+    results_path=script_path + "/data/measurements/json",
+    reload=RELOAD_DATA)
